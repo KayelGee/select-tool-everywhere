b2df0b98d943540f45cd6a1b2ca3857f956df851
KayelGee <kayelGee2@gmail.com>
2020-07-30 14:14:52 +0200

    Move select tool to the top Only add the select tool if no one else has done so before(including core) Show border when selected and keep border even after hover

--- select-tool-everywhere.js
+++ select-tool-everywhere.js
@@ -14,31 +14,51 @@
 		static _getControlButtons(controls){
 			for (let i = 0; i < controls.length; i++) {
 				if(controls[i].name === "lighting"){
-					controls[i].tools.push({
-						name: "select",
-						title: "CONTROLS.LightSelect",
-						icon: "fas fa-expand"
-					});
+					if (!controls[i].tools.find(tool => tool.name === "select")) {
+						controls[i].tools.unshift({
+							name: "select",
+							title: "CONTROLS.LightSelect",
+							icon: "fas fa-expand"
+						});
+					}
 				}
 				else if(controls[i].name === "sounds"){
-					controls[i].tools.push({
-						name: "select",
-						title: "CONTROLS.SoundSelect",
-						icon: "fas fa-expand"
-					});
+					if (!controls[i].tools.find(tool => tool.name === "select")) {
+						controls[i].tools.unshift({
+							name: "select",
+							title: "CONTROLS.SoundSelect",
+							icon: "fas fa-expand"
+						});
+					}
 				}
 				else if(controls[i].name === "measure"){
-					controls[i].tools.push({
-						name: "select",
-						title: "CONTROLS.TemplateSelect",
-						icon: "fas fa-expand"
-					});
+					if (!controls[i].tools.find(tool => tool.name === "select")) {
+						controls[i].tools.unshift({
+							name: "select",
+							title: "CONTROLS.TemplateSelect",
+							icon: "fas fa-expand"
+						});
+					}
 				}
 			}
 			console.log("SelectToolEverywhere | Tools added.");
 		}
+
+		static placeableSelected(placeable, selected){
+            placeable.controlIcon.border.visible = selected
+		}
+		static placeableHovered(placeable, hovered){
+			if(placeable._controlled){
+				placeable.controlIcon.border.visible = true
+			}
+		}
+
 	}
 
 	Hooks.on('getSceneControlButtons', (controls) => SelectToolEverywhere._getControlButtons(controls));
 	Hooks.on('canvasReady', () => SelectToolEverywhere.initialize());
+	for (const type of ["AmbientLight", "AmbientSound", "MeasuredTemplate", "Note"]) {
+		Hooks.on(`control${type}`, SelectToolEverywhere.placeableSelected);
+		Hooks.on(`hover${type}`, SelectToolEverywhere.placeableHovered);
+	}
 })();
