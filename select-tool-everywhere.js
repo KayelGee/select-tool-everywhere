7e815100423454da6393e11ab90895ec773e3da3
Aedif <vaitkus.martynas@gmail.com>
2022-10-19 23:14:49 +0100

    Formatting

--- select-tool-everywhere.js
+++ select-tool-everywhere.js
@@ -1,78 +1,79 @@
 (async () => {
-	class SelectToolEverywhere {
-		static initialize(){
-			//Patch so select tool works for Light and Sound
-			canvas.getLayerByEmbeddedName("AmbientLight").options.controllableObjects = true;
-			canvas.getLayerByEmbeddedName("AmbientSound").options.controllableObjects = true;
-			canvas.getLayerByEmbeddedName("MeasuredTemplate").options.controllableObjects = true;
-		}
+  class SelectToolEverywhere {
+    static initialize() {
+      //Patch so select tool works for Light and Sound
+      canvas.getLayerByEmbeddedName("AmbientLight").options.controllableObjects = true;
+      canvas.getLayerByEmbeddedName("AmbientSound").options.controllableObjects = true;
+      canvas.getLayerByEmbeddedName("MeasuredTemplate").options.controllableObjects = true;
+    }
 
-		/**
-		 * Hook into the toolbar and add buttons 
-		 */
-		static _getControlButtons(controls){
-			let added_tools=[];
-			for (let i = 0; i < controls.length; i++) {
-				if(controls[i].name === "lighting"){
-					if (!controls[i].tools.find(tool => tool.name === "select")) {
-						controls[i].tools.unshift({
-							name: "select",
-							title: "CONTROLS.LightSelect",
-							icon: "fas fa-expand"
-						});
-						added_tools.push("AmbientLight");
-					}
-				}
-				if(controls[i].name === "sounds"){
-					if (!controls[i].tools.find(tool => tool.name === "select")) {
-						controls[i].tools.unshift({
-							name: "select",
-							title: "CONTROLS.SoundSelect",
-							icon: "fas fa-expand"
-						});
-						added_tools.push("AmbientSound");
-					}
-				}
-				else if(controls[i].name === "measure"){
-					if (!controls[i].tools.find(tool => tool.name === "select")) {
-						controls[i].tools.unshift({
-							name: "select",
-							title: "CONTROLS.TemplateSelect",
-							icon: "fas fa-expand"
-						});
-						added_tools.push("MeasuredTemplate");
-					}
-				}
-			}
-			window['select-tool-everywhere']=added_tools;
-			console.log("SelectToolEverywhere | Tools added.");
-		}
+    /**
+     * Hook into the toolbar and add buttons
+     */
+    static _getControlButtons(controls) {
+      let added_tools = [];
+      for (let i = 0; i < controls.length; i++) {
+        if (controls[i].name === "lighting") {
+          if (!controls[i].tools.find((tool) => tool.name === "select")) {
+            controls[i].tools.unshift({
+              name: "select",
+              title: "CONTROLS.LightSelect",
+              icon: "fas fa-expand",
+            });
+            added_tools.push("AmbientLight");
+          }
+        }
+        if (controls[i].name === "sounds") {
+          if (!controls[i].tools.find((tool) => tool.name === "select")) {
+            controls[i].tools.unshift({
+              name: "select",
+              title: "CONTROLS.SoundSelect",
+              icon: "fas fa-expand",
+            });
+            added_tools.push("AmbientSound");
+          }
+        } else if (controls[i].name === "measure") {
+          if (!controls[i].tools.find((tool) => tool.name === "select")) {
+            controls[i].tools.unshift({
+              name: "select",
+              title: "CONTROLS.TemplateSelect",
+              icon: "fas fa-expand",
+            });
+            added_tools.push("MeasuredTemplate");
+          }
+        }
+      }
+      window["select-tool-everywhere"] = added_tools;
+      console.log("SelectToolEverywhere | Tools added.");
+    }
 
-		static placeableSelected(placeable, selected){
-			if(!window['select-tool-everywhere'].find(type => type === placeable.layer.constructor.documentName)) return;
-            placeable.controlIcon.border.visible = selected;
-		}
-		static placeableHovered(placeable, hovered){
-			if(!window['select-tool-everywhere'].find(type => type === placeable.layer.constructor.documentName)) return;
-			if(placeable._controlled){
-				placeable.controlIcon.border.visible = true;
-			}
-		}
-
-	}
-	window['select-tool-everywhere']=[];
-	Hooks.on('getSceneControlButtons', (controls) => SelectToolEverywhere._getControlButtons(controls));
-	Hooks.on('canvasReady', () => SelectToolEverywhere.initialize());
-	for (const type of ["AmbientSound", "MeasuredTemplate"]) {
-		Hooks.on(`control${type}`, SelectToolEverywhere.placeableSelected);
-		Hooks.on(`hover${type}`, SelectToolEverywhere.placeableHovered);
-	}
-    Hooks.once("init", () => {
-		libWrapper.register("select-tool-everywhere", "AmbientLight.prototype._onDragLeftCancel",
-			function (...args) {
-				Object.getPrototypeOf(AmbientLight).prototype._onDragLeftCancel.apply(this, ...args);
-				this.updateSource({ defer: true });
-			}, "OVERRIDE"
-		);
-    });
+    static placeableSelected(placeable, selected) {
+      if (!window["select-tool-everywhere"].find((type) => type === placeable.layer.constructor.documentName)) return;
+      placeable.controlIcon.border.visible = selected;
+    }
+    static placeableHovered(placeable, hovered) {
+      if (!window["select-tool-everywhere"].find((type) => type === placeable.layer.constructor.documentName)) return;
+      if (placeable._controlled) {
+        placeable.controlIcon.border.visible = true;
+      }
+    }
+  }
+  window["select-tool-everywhere"] = [];
+  Hooks.on("getSceneControlButtons", (controls) => SelectToolEverywhere._getControlButtons(controls));
+  Hooks.on("canvasReady", () => SelectToolEverywhere.initialize());
+  for (const type of ["AmbientSound", "MeasuredTemplate"]) {
+    Hooks.on(`control${type}`, SelectToolEverywhere.placeableSelected);
+    Hooks.on(`hover${type}`, SelectToolEverywhere.placeableHovered);
+  }
+  Hooks.once("init", () => {
+    libWrapper.register(
+      "select-tool-everywhere",
+      "AmbientLight.prototype._onDragLeftCancel",
+      function (...args) {
+        Object.getPrototypeOf(AmbientLight).prototype._onDragLeftCancel.apply(this, args);
+        this.updateSource({ defer: true });
+      },
+      "OVERRIDE"
+    );
+  });
 })();
