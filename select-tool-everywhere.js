12fbc8bb4774ea18e03885cf0a7490c9343cdffa
Aedif <vaitkus.martynas@gmail.com>
2022-10-20 18:29:16 +0100

    - Enable note controls - Fix note control border disappearing when being repositioned

--- select-tool-everywhere.js
+++ select-tool-everywhere.js
@@ -5,6 +5,7 @@
       canvas.getLayerByEmbeddedName("AmbientLight").options.controllableObjects = true;
       canvas.getLayerByEmbeddedName("AmbientSound").options.controllableObjects = true;
       canvas.getLayerByEmbeddedName("MeasuredTemplate").options.controllableObjects = true;
+      canvas.getLayerByEmbeddedName("Note").options.controllableObjects = true;
     }
 
     /**
@@ -22,8 +23,7 @@
             });
             added_tools.push("AmbientLight");
           }
-        }
-        if (controls[i].name === "sounds") {
+        } else if (controls[i].name === "sounds") {
           if (!controls[i].tools.find((tool) => tool.name === "select")) {
             controls[i].tools.unshift({
               name: "select",
@@ -48,18 +48,25 @@
     }
 
     static placeableRefresh(placeable) {
-      if (!window["select-tool-everywhere"].find((type) => type === placeable.layer.constructor.documentName)) return;
-      if (placeable.controlled) {
-        placeable.controlIcon.border.visible = true;
-      }
+      if (placeable.controlled) placeable.controlIcon.border.visible = true;
     }
   }
   window["select-tool-everywhere"] = [];
   Hooks.on("getSceneControlButtons", (controls) => SelectToolEverywhere._getControlButtons(controls));
   Hooks.on("canvasReady", () => SelectToolEverywhere.initialize());
-  for (const type of ["AmbientSound", "MeasuredTemplate", "AmbientLight"]) {
+  for (const type of ["AmbientSound", "MeasuredTemplate", "AmbientLight", "Note"]) {
     Hooks.on(`refresh${type}`, SelectToolEverywhere.placeableRefresh);
   }
+
+  // For notes the refresh hook is called while ControlIcon is still loading its texture (see ControlIcon.draw())
+  // Once the texture is loaded border visibility will be reset to false undoing our change in SelectToolEverywhere.placeableRefresh
+  // Instead delay here to account for this
+  Hooks.on("drawNote", (note) => {
+    setTimeout(() => {
+      SelectToolEverywhere.placeableRefresh(note);
+    }, 10);
+  });
+
   Hooks.once("init", () => {
     libWrapper.register(
       "select-tool-everywhere",
